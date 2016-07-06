067df88bfbf89ecc2d3827ae60f068dc4bbded9b
Aspirationtocode <aspiretocode@gmail.com>
2016-07-06 18:49:15 +0300

    first commit

---
+++ task1/app/js/common.js
@@ -0,0 +1,43 @@
+$(function() {
+
+
+	$('.section-main-content').flowtype({
+	   minFont   : 18,
+	   maxFont   : 22,
+	   fontRatio : 50
+	});
+	
+
+
+
+
+
+
+
+
+	//SVG Fallback
+	if(!Modernizr.svg) {
+		$("img[src*='svg']").attr("src", function() {
+			return $(this).attr("src").replace(".svg", ".png");
+		});
+	};
+
+	//E-mail Ajax Send
+	//Documentation & Example: https://github.com/agragregra/uniMail
+	$("form").submit(function() { //Change
+		var th = $(this);
+		$.ajax({
+			type: "POST",
+			url: "mail.php", //Change
+			data: th.serialize()
+		}).done(function() {
+			alert("Thank you!");
+			setTimeout(function() {
+				// Done Functions
+				th.trigger("reset");
+			}, 1000);
+		});
+		return false;
+	});
+
+});
